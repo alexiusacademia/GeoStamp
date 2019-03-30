a87f9183a3993601f7a6ec093f6bae56a5890917
Alexius Academia <alexius.academia@gmail.com>
2019-03-30 18:37:04 +0800

    Added fragment for settings.

--- app/src/main/java/com/alexiusacademia/geotagstamp/SettingsActivity.kt
+++ app/src/main/java/com/alexiusacademia/geotagstamp/SettingsActivity.kt
@@ -1,12 +1,24 @@
 package com.alexiusacademia.geotagstamp
 
+import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.alexiusacademia.geotagstamp.databinding.ActivitySettingsBinding
 
 class SettingsActivity : AppCompatActivity() {
 
+    lateinit var binding: ActivitySettingsBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_settings)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_settings)
+
+        if (savedInstanceState != null) {
+            return
+        }
+
+        fragmentManager.beginTransaction().
+            add(R.id.frame_settings_fragment_container, SettingsFragment()).
+            commit()
     }
 }
