8e290aabe4d8d75ed02463299f2cf9096e553b6f
Alexius Academia <alexius.academia@gmail.com>
2019-04-06 19:58:06 +0800

    Updated the SettingsFragment class to use PreferenceFragmentCompat.

--- app/src/main/java/com/alexiusacademia/geotagstamp/SettingsFragment.kt
+++ app/src/main/java/com/alexiusacademia/geotagstamp/SettingsFragment.kt
@@ -1,12 +1,22 @@
 package com.alexiusacademia.geotagstamp
 
 import android.os.Bundle
-import android.preference.PreferenceFragment
+import android.support.v7.preference.PreferenceFragmentCompat
 
-class SettingsFragment : PreferenceFragment() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+
+class SettingsFragment : PreferenceFragmentCompat() {
+
+
+    /*override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         addPreferencesFromResource(R.xml.preferences_layout)
+    }*/
+
+    // Replaced the onCreate method because of using the fragment to the existing layout
+    // instead of using a separate layout for the fragment
+    override fun onCreatePreferences(p0: Bundle?, p1: String?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        setPreferencesFromResource(R.xml.preferences_layout, p1)
     }
 }
